57edfd87c12a0660a3ed84541db9acc575b6979b
Manuel Olguin <manuel@olguin.se>
2019-10-02 19:58:07 -0400

    release 0.0.1

--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ from setuptools import find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+pkg_name = 'ganglion-biosensing'
+version = '0.0.1'
+
 setup(
-    name='ganglion-biosensing',
+    name=pkg_name,
     packages=find_packages(exclude='examples'),
-    version='0.0.1',
+    version=version,
     license='MIT',
     description='Modern Python 3.7+ library for interfacing with the OpenBCI '
                 'Ganglion biosensing board over BTLE.',
@@ -16,8 +19,9 @@ setup(
     long_description_content_type="text/markdown",
     author='Manuel Olguin Munoz',
     author_email='manuel@olguin.se',
-    url='https://github.com/molguin92/ganglion-biosensing',
-    # download_url='',
+    url=f'https://github.com/molguin92/{pkg_name}',
+    download_url=f'https://github.com/molguin92/'
+                 f'{pkg_name}/archive/{version}.tar.gz',
     keywords=['device', 'control', 'eeg', 'emg', 'ekg', 'ads1299', 'openbci',
               'ganglion'],
     install_requires=['numpy', 'bitstring', 'bluepy'],
